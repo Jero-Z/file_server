49ca4e7413682cab70b0693b18b8ceb2a4cf533e
xiaojie.zhu <andy771480011@gmail.com>
2017-07-17 10:08:50 +0800

    微信API 证书上传.图片资源上传重构

--- public/index.php
+++ public/index.php
@@ -5,14 +5,19 @@
  * 2. 前端进行通过 /crop 接口裁切， crop接口返回新的临时图片地址
  * 3. 前端确定裁切完成 通过/save 用临时文件地址交换永久图片地址
  *
+ * wechat api证书上传
+ * 1. /fileUpload 接口上传 判断是否为私密上传
+ * 2. 上传成功后返回私密地址
  *
  * 上传目录
  * 永久文件保存到files/contexts/{context}目录 context值由/save传入，默认为为default
  * 临时文件保存到files/temp目录
+ * wechat api 文件保存到wechat_api_file
  */
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Http\UploadedFile;
 use Intervention\Image\ImageManagerStatic as Image;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -28,6 +33,8 @@ $container['host'] = $_SERVER["HTTP_HOST"];
 $container['tmp_file_dir'] = '/files/temp';
 //永久文件目录
 $container['files_contexts_dir'] = '/files/contexts/';
+//微信API 存储地址
+$container['wechat_api_file_path'] = 'wechat_api_file';
 
 $app->get('/', function (Request $req, Response $res, $args = []) {
     return 'file-server';
@@ -35,16 +42,35 @@ $app->get('/', function (Request $req, Response $res, $args = []) {
 //创建文件路径
 function Directory($dir)
 {
-
     return is_dir($dir) or Directory(dirname($dir)) and mkdir($dir, 0777);
 
 }
 
-//url转为系统路径
-function convertUrl($url,$host)
+//判断路径是否存在
+function dirIsExists($path)
 {
-   return  WEB_ROOT . mb_substr($url, strlen($host));
+    if (!file_exists($path)) {
+        Directory($path);
+    }
 }
+
+//url转为系统路径
+function convertUrl($url, $host)
+{
+    return WEB_ROOT . mb_substr($url, strlen($host));
+}
+
+//判断上传资源是否合法
+function isLawful($uploadedFiles, $res)
+{
+    if (!is_array($uploadedFiles) || empty($uploadedFiles)) {
+        $data['message'] = '上传有误，请重新上传';
+        return $res->withJson($data);
+    }
+    //弹出上传数据为一维数组
+    return array_pop($uploadedFiles);
+}
+
 /*
  * 上传图片文件 返回临时图片地址
   * @param resource
@@ -55,9 +81,7 @@ $app->post('/upload', function (Request $req, Response $res, $args = []) {
     $temp_file_name = $this->get('tmp_file_dir') . '/' . uniqid() . '.png';
     $temp_file_path = WEB_ROOT . $temp_file_name;
     //判断文件夹是否存在
-    if (!file_exists(WEB_ROOT.$this->get('tmp_file_dir'))) {
-        Directory(WEB_ROOT.$this->get('tmp_file_dir'));
-    }
+    dirIsExists(WEB_ROOT . $this->get('tmp_file_dir'));
 
     // 图片文件全部转为png
     $allow_type = [
@@ -74,13 +98,7 @@ $app->post('/upload', function (Request $req, Response $res, $args = []) {
     //获取上传资源
     $uploadedFiles = $req->getUploadedFiles();
 
-    //判定资源是否合法
-    if (!is_array($uploadedFiles) || empty($uploadedFiles)) {
-        $data['message'] = '上传有误，请重新上传';
-        return $res->withJson($data);
-    }
-    //弹出上传数据为一维数组
-    $file = array_pop($uploadedFiles);
+    $file = isLawful($uploadedFiles, $res);
 
     $resource = Image::make($file->file);
 
@@ -91,6 +109,7 @@ $app->post('/upload', function (Request $req, Response $res, $args = []) {
     }
     //验证通过后转为PNG类型图片 存储于临时目录下
     $result = $resource->save($temp_file_path);
+
     if (!$result) {
         $data['message'] = 'error';
         $data['url'] = '';
@@ -122,7 +141,8 @@ $app->post('/crop', function (Request $req, Response $res, $args = []) {
      * 3.返回临时图片地址
      */
     $parsedBody = $req->getParsedBody();
-    $path = convertUrl($parsedBody['url'],$this->get('host'));
+
+    $path = convertUrl($parsedBody['url'], $this->get('host'));
     //异常处理
     try {
         $image = Image::make($path);
@@ -165,7 +185,7 @@ $app->post('/save', function (Request $req, Response $res, $args = []) {
     // todo
     $params = $req->getParsedBody();
 
-    $path = convertUrl($params['temp_path'],$this->get('host'));
+    $path = convertUrl($params['temp_path'], $this->get('host'));
     //用户选择保存路径用户
     $save_name = $params['context'] ? $params['context'] : 'default';
     //生成永久链接地址 路径
@@ -173,11 +193,10 @@ $app->post('/save', function (Request $req, Response $res, $args = []) {
     $permanent_file_url = $_SERVER["HTTP_HOST"] . $permanent_file_path;
 
     //文件目录创建
-    if (!file_exists(WEB_ROOT.$this->get('files_contexts_dir').$save_name)) {
-        Directory(WEB_ROOT.$this->get('files_contexts_dir').$save_name);
-    }
+    dirIsExists(WEB_ROOT . $this->get('files_contexts_dir') . $save_name);
+
     //生成永久文件
-    $result = Image::make($path)->save(WEB_ROOT.$permanent_file_path);
+    $result = Image::make($path)->save(WEB_ROOT . $permanent_file_path);
 
     //返回结果
     if (!$result) {
@@ -193,5 +212,50 @@ $app->post('/save', function (Request $req, Response $res, $args = []) {
 
 });
 
+/*
+ *  上传微信API证书
+ *  @param file
+ *  @return private url path
+ */
+$app->post('/fileUpload', function (Request $req, Response $res, $args = []) {
+    //获取用户携带token
+    /*
+     * 判断token是否合法
+    if (!true) {
+        $data['message'] = 'User unverified';
+        $data['name'] = '';
+        return $res->withJson($data);
+    }
+    */
+    //文件唯一ID
+    $uniqid = uniqid();
+
+    $uploadedFiles = $req->getUploadedFiles();
+    //判断文件夹是否存在
+//    var_dump($_SERVER["DOCUMENT_ROOT"]);die;
+    dirIsExists(WEB_ROOT . '../../' . $this->get('wechat_api_file_path'));
+    //判断文件是否合法
+    $file = isLawful($uploadedFiles, $res);
+    //获取文件名
+    $fileName = $file->getClientFilename();
+    //生成唯一文件名
+    $uniquenessName = $uniqid . substr($fileName, strrpos($fileName, '.'));
+    //生成保存地址
+    $filePath = WEB_ROOT . '../../' . $this->get('wechat_api_file_path') . '/' . $uniquenessName;
+    //返回json数据
+    try {
+
+        $file->moveTo($filePath);
+        $data['message'] = 'success';
+        $data['name'] = $uniquenessName;
+        return $res->withJson($data);
+
+    } catch (Exception $exception) {
+        $data['message'] = 'error';
+        $data['name'] = '';
+        return $res->withJson($data);
+    }
+
+});
 
 $app->run();
